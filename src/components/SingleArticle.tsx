e95ece4fdac245af95364cc4b45ce0ca261f37db
Diana <dianadorojuc3@gmail.com>
2024-09-05 15:09:25 +0200

    commit detaills

--- src/components/SingleArticle.tsx
+++ src/components/SingleArticle.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Col,Card,Button } from "react-bootstrap"
+import { Col,Card,Button,Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import AboutArticles from "../details/interfaces"
 
@@ -30,28 +30,28 @@ useEffect(() => {
 }, [])
 
 
-return(
-    <Col> {singleArticle.map((article) => {
-        return(
-            <Card key = {article.id} className="mb-4">
-            <Card.Img variant="top" src={article.image_url} />
+return (
+    <Row className="g-4"> 
+      {singleArticle.map((article) => (
+        <Col key={article.id} xs={12} md={4} lg={3}>
+          <Card className="mb-4 my-card h-100">
+            <Card.Img variant="top" src={article.image_url} className="h-100"/>
             <Card.Body>
               <Card.Title>{article.title}</Card.Title>
               <Card.Text>
-              {article.summary}
+                <small className="text-muted">Pubblicato il {new Date(article.published_at).toLocaleDateString()}</small>
               </Card.Text>
-              <Card.Text>
-              <small className="text-muted">Pubblicato il {new Date(article.published_at).toLocaleDateString()}</small>
-            </Card.Text>
-            <Link to={`/articles/${article.id}`}>
-              <Button variant="primary">Leggi di più</Button>
-            </Link>
+              <Link to={`/articles/${article.id}`}>
+                <div className="d-flex justify-content-center">
+                  <Button variant="success">Vai ai dettagli</Button>
+                </div>
+              </Link>
             </Card.Body>
           </Card>
-        )
-    })}
-   
-    </Col>
-)
-}
-export default SingleArticle
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default SingleArticle;
